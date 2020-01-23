67f91fb5dc055b313946e83ed1df014773140f57
Tamim <tufaruk7@gmail.com>
2020-01-22 19:26:57 -0500

    Added base logic for IMU messaging publishing.

--- scripts/hardware/imu_driver.py
+++ scripts/hardware/imu_driver.py
@@ -8,18 +8,29 @@ IMU msgs to "imu" channel.
 """
 
 import rospy
-from guard.msg import IMU
+from sensor_msgs.msg import Imu
 
 def pub_data():
-    pub = rospy.Publisher('imu', IMU, queue_size=10)
+    pub_imu = rospy.Publisher('imu', Imu, queue_size=10)
     rospy.init_node('imu_hardware', anonymous=True)
     rate = rospy.Rate(10)
+    
+    imu_msg = Imu()
+    imu_msg.header.stamp = rospy.Time.now()
 
-    # interface with serial connection to arduino
+    # interface with serial connection ...
+
+    # rospy.loginfo("Ready for publishing imu:" + serial_port)
 
     while not rospy.is_shutdown():
         # fill msg with imu data
+        rospy.loginfo(imu_msg)
+        pub_imu.publish(imu_msg)
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
+    	pub_data()
+    except rospy.ROSInterruptException:
+    	pass
         
