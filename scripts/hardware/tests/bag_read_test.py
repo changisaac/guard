cb6ed0be89e7b9c9d43f59b4dd09b3236759ce10
Isaac Chang <chang.isaac97@gmail.com>
2020-03-03 02:47:14 -0500

    modified bag read test to work on images

--- scripts/hardware/tests/bag_read_test.py
+++ scripts/hardware/tests/bag_read_test.py
@@ -1,10 +1,13 @@
 import rosbag
-import pdb
+from cv_bridge import CvBridge 
+import cv2
 
-bag = rosbag.Bag("test.bag")
-bag.reindex()
+bag = rosbag.Bag("../guard_cam_1_rgb_image_1583219388214226007.bag")
+bridge = CvBridge()
 
-for topic, msg, t in bag.read_messages(topics=["/test_topic"]):
-    print(topic, msg, t)
+for topic, msg, t in bag.read_messages(topics=["/cam_1/color/image_raw"]):
+    image = bridge.imgmsg_to_cv2(msg, "bgr8")
+    cv2.imshow("Image Window", image)
+    cv2.waitKey(50)
 
-    bag.close()
+bag.close()
