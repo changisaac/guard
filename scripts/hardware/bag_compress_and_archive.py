6cceac919d4ec5a108ee2eda8fd02b421de04106
Tamim <tufaruk7@gmail.com>
2020-03-03 23:49:04 -0500

    Added the ability to move the archives in a selected folder. Created bash script.

--- scripts/hardware/bag_compress_and_archive.py
+++ scripts/hardware/bag_compress_and_archive.py
@@ -5,18 +5,30 @@ from time import gmtime, strftime
 import argparse
 
 default_bag_directory = "/home/tamim/bagfiles/"
+default_archive_directory = "/home/tamim/archive"
 
 # Need to make this one a string, presumably 
 # because shell=True for this command
 cmd_compress = "rosbag compress --output-dir=%s *.bag" 
 cmd_archive = (["tar", "cfv", "guard_user_%s_%s.tar"])
+cmd_mv_archive = (["mv"])
+
+def collectArguments():
+	parser = argparse.ArgumentParser(description='Compress and archive bag files')
+	parser.add_argument('--user', '-u', default='default', help='Name to append on archive')
+	parser.add_argument('--directory', '-d', default=default_bag_directory,
+						help='Absolute path of directory where bag files are stored')
+	parser.add_argument('--archive', '-a', default=default_archive_directory,
+		                help='Absolute path of directory where archives should be stored')
+	args = parser.parse_args()
+
+	return args.user, args.directory, args.archive
 
 def compress(directory, compressed_directory):
 	print "CMD: ", cmd_compress
 	try:
 		print "CHANGING TO DIRECTORY %s" % directory
 		os.chdir(directory)
-		sp.call("ls")
 		print "COMPRESSING BAG FILES"
 		sp.call(cmd_compress % compressed_directory, shell=True)
 	except:
@@ -24,11 +36,17 @@ def compress(directory, compressed_directory):
 
 	print "COMPRESSION COMPLETE"
 
-def archive(user, directory):
+def archive(user, directory, archive_directory):
 	cmd_archive[2] = cmd_archive[2] % (user, strftime("%Y-%m-%d_%H_%M_%S", gmtime()))
+	
+	# Set the archive filename and directory it should be moved to
+	cmd_mv_archive.append(cmd_archive[2])
+	cmd_mv_archive.append(archive_directory)
 
+	print "MOVE CMD: ", cmd_mv_archive
+
+	# Select all the files to archive
 	os.chdir(directory)
-	sp.call("ls")
 	for filename in os.listdir(directory):
 	    if filename.endswith('.bag'):
 	        cmd_archive.append(filename)
@@ -39,21 +57,18 @@ def archive(user, directory):
 	except:
 		print "ARCHIVE FAILED"
 
-def collectArguments():
-	parser = argparse.ArgumentParser(description='Compress and archive bag files')
-	parser.add_argument('--user', '-u', default='default', help='Name to append on archive')
-	parser.add_argument('--directory', '-d', default=default_bag_directory,
-						help='Absolute path of directory where bag files are stored')
-	args = parser.parse_args()
-
-	return args.user, args.directory
+	# Move archive to archive folder
+	try:
+		sp.call(cmd_mv_archive)
+	except:
+		print "COULD NOT MOVE ARCHIVE"
 
 def main():
-	user, directory = collectArguments()
+	user, directory, archive_directory = collectArguments()
 	compressed_directory = directory + '/compressed'
-	
+
 	compress(directory, compressed_directory)
-	archive(user, compressed_directory)
+	archive(user, compressed_directory, archive_directory)
 
 if __name__ == '__main__':
 	main()
